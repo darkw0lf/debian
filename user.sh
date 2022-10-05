a95723b61c982f545491b22db4e63302e8bfc0d4
Pascal Sené <38534891+darkw0lf@users.noreply.github.com>
2022-10-05 16:19:11 +0200

    Update user.sh

--- user.sh
+++ user.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 groupadd --gid 10000 formation
 mkdir -p /documents/travail
-useradd --home-dir /documents/travail --gid 10000 --uid 10000 formation
+useradd --home-dir /documents/travail --gid 10000 --uid 10000 --shell /bin/bash formation
 chown -R formation:formation /documents/travail
 chmod -R 755 /documents/travail
