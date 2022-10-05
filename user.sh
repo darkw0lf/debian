12cc0483fa5ff34b44a3c5a329de3d95d7fa6fdb
Pascal Sené <38534891+darkw0lf@users.noreply.github.com>
2022-10-05 16:14:43 +0200

    Create user.sh

---
+++ user.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+groupadd --gid 10000 formation
+mkdir -p /documents/travail
+useradd --home-dir /documents/travail --gid 10000 --uid 10000 formation
+chown -R formation:formation /documents/travail
+chmod -R 755 /documents/travail
