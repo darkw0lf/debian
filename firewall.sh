23233c74d435fa2d1ca8faa6dc994a8bee25c876
Pascal Sené <38534891+darkw0lf@users.noreply.github.com>
2024-05-30 15:06:58 +0200

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-### BEGIN INIT INFO
-# Provides:          server
-# Required-Start:    $network
-# Required-Stop:     $network
-# Default-Start:     2 3 4 5
-# Default-Stop:      0 1 6
-# Short-Description: NAT
-# Description:       Active le NAT et le firewall
-### END INIT INFO
-
 # Vider les tables actuelles
 iptables -t filter -F
 
@@ -22,41 +12,41 @@ iptables -t filter -P FORWARD ACCEPT
 iptables -t filter -P OUTPUT ACCEPT
 
 # Interdire IPs
-#iptables -A INPUT -s -j DROP
+#iptables -A INPUT -s 198.27.83.122 -j REJECT
 
 # Ne pas casser les connexions etablies
 iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 
 # INTERNET
-iptables -I INPUT -d IP_INTERNET -p tcp --dport 80 -m string --to 700  --algo bm --string 'Host: IP_INTERNET' -j DROP
-iptables -I INPUT -d IP_INTERNET -p tcp --dport 80 -m string --to 70 --algo bm --string 'GET /w00tw00t.at.ISC.SANS.' -j DROP
-iptables -A INPUT -p tcp --dport 80 -m state --state NEW -m recent --name BLACKLIST --set
-iptables -A INPUT -p tcp --dport 80 -m state --state NEW -m recent --name BLACKLIST --update --seconds 10 --hitcount 10 --rttl -j DROP
+#iptables -I INPUT -p tcp --dport 80 -m string --to 700 --algo bm --string 'Host: 51.158.152.141' -j DROP
+#iptables -I INPUT -p tcp --dport 80 -m string --to 700 --algo bm --string 'Host: 195.154.50.159' -j DROP
+
+# ROBOTS
+#iptables -I INPUT -d 51.158.152.141 -p tcp --dport 80 -m string --to 70 --algo bm --string 'GET /w00tw00t.at.ISC.SANS.' -j DROP
+#iptables -I INPUT -d 195.154.50.159 -p tcp --dport 80 -m string --to 70 --algo bm --string 'GET /w00tw00t.at.ISC.SANS.' -j DROP
+
+# DDOS
+#iptables -A INPUT -p tcp -m state --state NEW -m recent --name BLACKLIST --set
+#iptables -A INPUT -p tcp -m state --state NEW -m recent --name BLACKLIST --update --seconds 10 --hitcount 10 --rttl -j DROP
 
 # LOOPBACK 
 iptables -t filter -A INPUT -i lo -j ACCEPT
 iptables -t filter -A OUTPUT -o lo -j ACCEPT
 
 # ICMP
+#iptables -t filter -A INPUT -p icmp -d 51.158.152.141 -j DROP
+#iptables -t filter -A INPUT -p icmp -d 195.154.50.159 -j DROP
 iptables -t filter -A INPUT -p icmp -j ACCEPT
 iptables -t filter -A OUTPUT -p icmp -j ACCEPT
 
 # SSH
-iptables -t filter -A INPUT -p tcp --dport 555 -j ACCEPT
-iptables -t filter -A OUTPUT -p tcp --dport 555 -j ACCEPT
+iptables -t filter -A INPUT -p tcp -d 51.158.152.141 --dport 555 -j ACCEPT
+iptables -t filter -A OUTPUT -p tcp -d 51.158.152.141 --dport 555 -j ACCEPT
 
 # DNS
-iptables -t filter -A OUTPUT -p tcp --dport 53 -j ACCEPT
-iptables -t filter -A OUTPUT -p udp --dport 53 -j ACCEPT
-iptables -t filter -A INPUT -p tcp --dport 53 -j ACCEPT
-iptables -t filter -A INPUT -p udp --dport 53 -j ACCEPT
-
-# FTP
-iptables -t filter -A INPUT -p tcp --dport 20:21 -j ACCEPT
-iptables -t filter -A OUTPUT -p tcp --dport 20:21 -j ACCEPT
-iptables -t filter -A INPUT -p tcp --dport 20000:20050 -j ACCEPT
-iptables -t filter -A OUTPUT -p tcp --dport 20000:20050 -j ACCEPT
+iptables -t filter -A OUTPUT -p tcp -d 51.158.152.141 --dport 53 -j ACCEPT
+iptables -t filter -A OUTPUT -p udp -d 51.158.152.141 --dport 53 -j ACCEPT
 
 # HTTP
 iptables -t filter -A INPUT -p tcp --dport 80 -j ACCEPT
@@ -67,45 +57,13 @@ iptables -t filter -A INPUT -p tcp --dport 443 -j ACCEPT
 iptables -t filter -A OUTPUT -p tcp --dport 443 -j ACCEPT
 
 # SMTP
-iptables -t filter -A INPUT -p tcp --dport 25 -j ACCEPT
-iptables -t filter -A OUTPUT -p tcp --dport 25 -j ACCEPT
+iptables -t filter -A INPUT -p tcp -d 51.158.152.141 --dport 25 -j ACCEPT
+iptables -t filter -A OUTPUT -p tcp -d 51.158.152.141 --dport 25 -j ACCEPT
 
-# IMAPS
-iptables -t filter -A INPUT -p tcp --dport 993 -j ACCEPT
-iptables -t filter -A OUTPUT -p tcp --dport 993 -j ACCEPT
+# IMAP
+iptables -t filter -A INPUT -p tcp -d 51.158.152.141 --dport 143 -j ACCEPT
+iptables -t filter -A OUTPUT -p tcp -d 51.158.152.141 --dport 143 -j ACCEPT
 
-# POP3
-iptables -t filter -A INPUT -p tcp --dport 110 -j ACCEPT
-iptables -t filter -A OUTPUT -p tcp --dport 110 -j ACCEPT
-
-# SPAMASSASSIN
-iptables -t filter -A INPUT -p tcp --dport 783 -j ACCEPT
-iptables -t filter -A OUTPUT -p tcp --dport 783 -j ACCEPT
-
-# NTP
-iptables -t filter -A OUTPUT -p udp --dport 123 -j ACCEPT
-
-# WEBMIN
-#iptables -t filter -A INPUT -p tcp --dport 10000 -j ACCEPT
-#iptables -t filter -A OUTPUT -p tcp --dport 10000 -j ACCEPT
-
-# VPN
-#iptables -t nat -A POSTROUTING -s RESEAU_PRIVE -o eth0 -j MASQUERADE
-#iptables -t nat -A POSTROUTING -o venet0 -j SNAT --to-source IP_INTERNET 
-#iptables -t filter -A OUTPUT -p udp --dport 1194 -j ACCEPT
-#iptables -t filter -A INPUT -p udp --dport 1194 -j ACCEPT
-####    Debut NAT       ####
-
-# Active l'ip forwarding
-echo 1 > /proc/sys/net/ipv4/ip_forward
-
-# NAT VM subnet to external ip
-#iptables -t nat -A POSTROUTING -s RESEAU_LOCAL -o eth0 -j SNAT --to 88.190.22.217
-
-# Allow all traffic for venet0 interface
-#iptables -A INPUT -i venet0 -j ACCEPT
-
-#iptables -t nat -I PREROUTING -p tcp -d IP_INTERNET --dport 80 -j DNAT --to IP_LOCAL:80
-#iptables -I FORWARD -p tcp -d IP_LOCAL --dport 80
-#iptables -t nat -I PREROUTING -p tcp -d IP_INTERNET --dport 443 -j DNAT --to IP_LOCAL:443
-#iptables -I FORWARD -p tcp -d IP_LOCAL --dport 443
+# WAZUH
+iptables -t filter -A INPUT -p tcp -d 51.158.152.141 --dport 55000 -j ACCEPT
+iptables -t filter -A OUTPUT -p tcp -d 51.158.152.141 --dport 55000 -j ACCEPT
