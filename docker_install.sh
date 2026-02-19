bcfd11708132eacb7aa64958c490cbdfc3dc116f
Pascal Sené <38534891+darkw0lf@users.noreply.github.com>
2026-02-19 09:03:22 +0100

    Create docker_install.sh for Docker installation
    Install Docker on Debian by adding the GPG key and repository.

---
+++ docker_install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+apt update
+apt install ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/debian
+Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+apt update
