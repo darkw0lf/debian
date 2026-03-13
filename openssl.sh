d09beb2eecada7ba3ea7a66c892f616c7fd3ad1c
Pascal Sené <38534891+darkw0lf@users.noreply.github.com>
2026-03-13 15:08:44 +0100

    Add OpenVPN installation script with dependencies

---
+++ openssl.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get install curl
+
+curl -O https://raw.githubusercontent.com/Angristan/openvpn-install/master/openvpn-install.sh
+
+chmod +x openvpn-install.sh
+
+sudo ./openvpn-install.sh
