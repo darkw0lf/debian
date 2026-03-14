75a99dfadd67e355f6411189293a60f6d17a3cb0
Pascal Sené <38534891+darkw0lf@users.noreply.github.com>
2026-03-14 13:24:08 +0100

    Update and rename wireguard.sh to wireguard_server.sh

--- wireguard_server.sh
+++ wireguard_server.sh
@@ -10,16 +10,3 @@ PostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -t nat -D POSTROUTING 
 [Peer]
 PublicKey  = <CLEF_PUBLIQUE_CLIENT>
 AllowedIPs = 10.8.0.2/32     # Seul cet IP est autorisé pour ce peer
-
-==========
-# /etc/wireguard/wg0.conf (CLIENT — 10.8.0.2)
-[Interface]
-Address    = 10.8.0.2/24
-PrivateKey = <CLEF_PRIVEE_CLIENT>
-DNS        = 10.8.0.1          # DNS via VPN (prévention DNS Leak)
-
-[Peer]
-PublicKey           = <CLEF_PUBLIQUE_SERVEUR>
-Endpoint            = vpn.entreprise.com:51820
-AllowedIPs          = 0.0.0.0/0    # Full Tunnel — tout le trafic
-PersistentKeepalive = 25            # Maintenir connexion derrière NAT
